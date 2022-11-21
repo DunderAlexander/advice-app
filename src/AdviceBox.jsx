1c491c1dece9436a9ca8ac9eff63088a62822125
DunderAlexander <mego.sanello@gmail.com>
2022-11-21 18:19:21 +0400

    finished mobile UI in CSS

---
+++ src/AdviceBox.jsx
@@ -0,0 +1,18 @@
+function AdviceBox() {
+  return (
+    <div className="wrapper">
+      <h4 className="title">ADVICE # 117</h4>
+      <p className="advice">
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur,
+        explicabo."
+      </p>
+      <img src="./images/pattern-divider-desktop.svg" alt="" />
+      <div className="dice">
+        <img src="./images/icon-dice.svg" alt="" />
+      </div>
+      {/* <img className="dice" src="./images/icon-dice.svg" alt="" /> */}
+    </div>
+  );
+}
+
+export default AdviceBox;
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AdviceBox from "./AdviceBox";
+export default function App() {
+  return (
+    <div className="container">
+      <AdviceBox />
+    </div>
+  );
+}
