9569d8b5af77824e4792c2193477c1d98f39deb1
DunderAlexander <mego.sanello@gmail.com>
2022-12-20 16:25:43 +0400

    changed image paths

--- src/AdviceBox.jsx
+++ src/AdviceBox.jsx
@@ -3,9 +3,9 @@ function AdviceBox({ slip, onClick }) {
     <div className="wrapper">
       <h4 className="title">ADVICE #{slip.slip.id}</h4>
       <p className="advice">{slip.slip.advice}</p>
-      <img src="./images/pattern-divider-desktop.svg" alt="" />
+      <img src="/pattern-divider-desktop.svg" alt="" />
       <div className="dice" onClick={onClick}>
-        <img src="./images/icon-dice.svg" alt="" />
+        <img src="/icon-dice.svg" alt="" />
       </div>
     </div>
   );
